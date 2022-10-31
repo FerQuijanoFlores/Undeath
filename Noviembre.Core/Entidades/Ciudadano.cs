8ae5a9a2fd1afa84b36d2ac2c3fe91ca8afcdc8a
tomat <tomat@DESKTOP-44P6VII>
2022-10-31 08:56:13 -0700

    31/10/22

--- Noviembre.Core/Entidades/Ciudadano.cs
+++ Noviembre.Core/Entidades/Ciudadano.cs
@@ -14,6 +14,7 @@ namespace Noviembre.Core.Entidades
         public string ApellidoPaterno { get; set; }
         public string ApellidoMaterno { get; set; }
         public string Telefono { get; set; }
+        public string Direccion { get; set; }
         public string Email { get; set; }
 
     }
--- Noviembre.Core/Entidades/Estado.cs
+++ Noviembre.Core/Entidades/Estado.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using Noviembre.Core.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +12,36 @@ namespace Noviembre.Core.Entidades
     {
         public int Id { get; set; }
         public string Nómbre { get; set; }
+
+        public static List<Estado> GetAll() { 
+        List<Estado> list = new List<Estado>();
+            try {
+            Conexion conexion = new Conexion();
+                if (conexion.OpenConnection()) {
+
+                    string query = "SELECT * FROM estado";
+
+                    MySqlCommand command = new MySqlCommand(query, conexion.connection);
+
+                    MySqlDataReader dataReader = command.ExecuteReader();
+                    while (dataReader.Read()) { 
+                    
+                        Estado estado = new Estado();
+                        estado.Id = int.Parse(dataReader["id"].ToString());
+                        estado.Nómbre = dataReader["nombre"].ToString();
+
+                        list.Add(estado);
+                    }
+                    dataReader.Close();
+                    conexion.CloseConnection();
+                }
+                ;
+            }catch(Exception ex) {
+                throw ex;
+            }
+
+            return list;
+        }
+
     }
 }
